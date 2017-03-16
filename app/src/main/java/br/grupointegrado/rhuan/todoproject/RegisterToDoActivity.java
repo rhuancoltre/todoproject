9a40bf609415f43c996e5925c3f786aecce83ec1
RHUAN\Presidencia Camara <rhuancoltre@gmail.com>
2017-03-15 22:39:41 -0300

    Commit inicial sem funcionalidades.

---
+++ app/src/main/java/br/grupointegrado/rhuan/todoproject/RegisterToDoActivity.java
@@ -0,0 +1,13 @@
+package br.grupointegrado.rhuan.todoproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterToDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_to_do);
+    }
+}
